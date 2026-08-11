cc167bf4e35a5e252720efc38be9bd94f8bc0cbb
rechachi mohamed chaouki <crypto.chaouki@gmail.com>
2026-08-11 07:20:27 +0100

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -4,28 +4,31 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require('path');
 
-// Stockage temporaire (en attendant une vraie DB)
-let scores = {}; // { pseudo: score }
-let chat = [];
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+let scores = {}; 
 
 io.on('connection', (socket) => {
     io.emit('playerCountUpdate', io.engine.clientsCount);
 
-    // Chat mondial
-    socket.on('sendMessage', (data) => {
-        const msg = { pseudo: data.pseudo, text: data.text };
-        chat.push(msg);
-        io.emit('newMessage', msg);
+    socket.on('setPseudo', (p) => socket.pseudo = p);
+
+    socket.on('playerTap', () => {
+        if(socket.pseudo) {
+            scores[socket.pseudo] = (scores[socket.pseudo] || 0) + 1;
+            io.emit('scoreUpdated', { pseudo: socket.pseudo, score: scores[socket.pseudo] });
+        }
     });
 
-    // Score mondial
-    socket.on('playerTap', (pseudo) => {
-        scores[pseudo] = (scores[pseudo] || 0) + 1;
-        io.emit('scoreUpdated', { pseudo: pseudo, score: scores[pseudo] });
+    socket.on('sendMessage', (data) => {
+        io.emit('newMessage', { pseudo: data.pseudo, text: data.text });
     });
 
     socket.on('disconnect', () => io.emit('playerCountUpdate', io.engine.clientsCount));
 });
 
-server.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => console.log(`Serveur actif sur port ${PORT}`));
